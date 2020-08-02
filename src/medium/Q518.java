a2c7279e702b63d8c9f87e0fc8f90cc573801327
pigzcz <33215823+pigzcz@users.noreply.github.com>
2020-08-02 21:45:39 +0800

    Q632

---
+++ src/medium/Q518.java
@@ -0,0 +1,57 @@
+package medium;
+
+import java.util.Arrays;
+
+/**
+ * 518. 零钱兑换 II
+ * 给定不同面额的硬币和一个总金额。写出函数来计算可以凑成总金额的硬币组合数。假设每一种面额的硬币有无限个。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: amount = 5, coins = [1, 2, 5]
+ * 输出: 4
+ * 解释: 有四种方式可以凑成总金额:
+ * 5=5
+ * 5=2+2+1
+ * 5=2+1+1+1
+ * 5=1+1+1+1+1
+ * 示例 2:
+ *
+ * 输入: amount = 3, coins = [2]
+ * 输出: 0
+ * 解释: 只用面额2的硬币不能凑成总金额3。
+ * 示例 3:
+ *
+ * 输入: amount = 10, coins = [10]
+ * 输出: 1
+ */
+public class Q518 {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
+        // base case
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++)
+                if (j - coins[i-1] >= 0)
+                    dp[i][j] = dp[i - 1][j]
+                            + dp[i][j - coins[i-1]];
+                else
+                    dp[i][j] = dp[i - 1][j];
+        }
+        return dp[n][amount];
+
+    }
+
+
+    public static void main(String[] args) {
+        Q518 q518 = new Q518();
+        int[] dd = new int[]{3,5,7,8,9,10,11};
+        int change = q518.change(100, dd);
+        System.out.println(change);
+    }
+}
