90e57a39545d92244fc0259cf61d96974f0873ed
pigzcz <33215823+pigzcz@users.noreply.github.com>
2020-08-14 00:56:34 +0800

    q100

--- src/medium/Q285.java
+++ src/medium/Q285.java
@@ -27,7 +27,9 @@ import java.util.List;
  * 解析: 因为给出的结点没有顺序后继，所以答案就返回 null 了。
  */
 public class Q285 {
-    public class TreeNode {
+    private boolean find = false;
+    private TreeNode result = null;
+    public static class TreeNode {
       int val;
       TreeNode left;
       TreeNode right;
@@ -59,4 +61,37 @@ public class Q285 {
         bianli(root.right,list);
     }
 
+    public TreeNode inorderSuccessor2(TreeNode root, TreeNode p) {
+        if (p == null){
+            return null;
+        }
+
+        bianli(root,p);
+        return result;
+    }
+    public void bianli(TreeNode root,TreeNode p){
+
+        if (null == root){
+            return;
+        }
+
+
+        bianli(root.left,p);
+        if (p.val < root.val && result == null){
+            result = root;
+            return;
+        }
+        bianli(root.right,p);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+        TreeNode left = new TreeNode(1);
+        TreeNode right = new TreeNode(3);
+        root.left = left;
+        root.right = right;
+        Q285 q285 = new Q285();
+        TreeNode treeNode = q285.inorderSuccessor2(root, left);
+        System.out.println(true);
+    }
 }
