16454fbb2504bb82e7a820078c59a4bf641f5bde
pigzcz <zcz_buaa@163.com>
2021-11-21 21:24:42 +0800

    abc

---
+++ src/study/ABC.java
@@ -0,0 +1,15 @@
+package study;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * @author:johnson.zhu
+ * @Date: 2021/11/21 9:23 下午
+ * @Description:
+ **/
+public class ABC {
+    public class ABCLock extends AbstractQueuedSynchronizer{
+        ˜
+
+    }
+}
